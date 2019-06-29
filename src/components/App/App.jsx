db31eded23d08eb5ccc2f1cc27970812a08e1794
Abuka <maacsgaming@gmail.com>
2019-06-29 16:03:40 +0300

    add new components

---
+++ src/components/App/App.jsx
@@ -0,0 +1,14 @@
+import React, { Fragment } from 'react'
+import Header from '../elements/Header/Header'
+import Home from '../Home/Home'
+
+const App = () => {
+  return (
+    <Fragment>
+      <Header />
+      <Home />
+    </Fragment>
+  )
+}
+
+export default App
