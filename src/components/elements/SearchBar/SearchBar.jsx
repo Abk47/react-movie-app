0da91c2670df29d7837ec90b7b1c175db98fb14e
Abuka <maacsgaming@gmail.com>
2019-06-29 22:40:34 +0300

    add component logic

---
+++ src/components/elements/SearchBar/SearchBar.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+import FontAwesome from 'react-fontawesome'
+import './SearchBar.css'
+
+class SearchBar extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+
+    }
+  }
+
+  render () {
+    return (
+      <div>
+          SearchBar
+      </div>
+    )
+  }
+}
+
+export default SearchBar
