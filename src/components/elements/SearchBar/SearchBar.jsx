79898e9bb7e898c06bee9c26a577f25eb018b1d5
Abuka <maacsgaming@gmail.com>
2019-06-30 19:14:58 +0300

    add search functions

--- src/components/elements/SearchBar/SearchBar.jsx
+++ src/components/elements/SearchBar/SearchBar.jsx
@@ -3,17 +3,34 @@ import FontAwesome from 'react-fontawesome'
 import './SearchBar.css'
 
 class SearchBar extends Component {
-  constructor (props) {
-    super(props)
-    this.state = {
-
-    }
+  state = {
+    value: ''
   }
 
-  render () {
+  timeout = null
+
+  doSearch = (e) => {
+    this.setState({ value: e.target.value })
+    clearTimeout(this.timeout)
+
+    this.timeout = setTimeout(() => {
+      this.props.callback(this.state.value)
+    }, 500)
+  }
+
+  render() {
     return (
-      <div>
-          SearchBar
+      <div className='rmdb-searchbar'>
+        <div className="rmdb-searchbar-content">
+          <FontAwesome className='rmdb-fa-search' name='search' size='2x' />
+          <input
+            type='text'
+            className='rmdb-searchbar-input'
+            placeholder='Search'
+            onChange={this.doSearch}
+            value={this.state.value}
+          />
+        </div>
       </div>
     )
   }
