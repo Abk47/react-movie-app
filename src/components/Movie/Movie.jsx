ffa23d9a13d40da0405ec68a69c31b970eb7470d
Abuka <maacsgaming@gmail.com>
2019-07-02 23:57:15 +0300

    completed components

--- src/components/Movie/Movie.jsx
+++ src/components/Movie/Movie.jsx
@@ -17,10 +17,15 @@ class Movie extends Component {
   }
 
   componentDidMount() {
-    this.setState({ loading: true })
-    // First fetch the movie data 
-    const endpoint = `${API_URL}movie/${this.props.match.params.movieId}?api_key=${API_KEY}&language=en-US`
-    this.fetchItems(endpoint)
+    if (localStorage.getItem(`${this.props.match.params.movieId}`)) {
+      const state = JSON.parse(localStorage.getItem(`${this.props.match.params.movieId}`))
+      this.setState({ ...state })
+    } else {
+      this.setState({ loading: true })
+      // First fetch the movie data 
+      const endpoint = `${API_URL}movie/${this.props.match.params.movieId}?api_key=${API_KEY}&language=en-US`
+      this.fetchItems(endpoint)
+    }
   }
 
   fetchItems = (endpoint) => {
@@ -43,11 +48,12 @@ class Movie extends Component {
                   actors: result.cast,
                   directors: directors,
                   loading: false
+                }, () => {
+                  localStorage.setItem(`${this.props.match.params.movieId}`, JSON.stringify(this.state))
                 })
               })
           })
         }
-
       })
       .catch(error => console.error('Error:', error))
   }
@@ -70,9 +76,9 @@ class Movie extends Component {
               })}
             </FourColGrid>
           </div>
-          : null }
-          {!this.state.actors && !this.state.loading ? <h1>No Movie Found</h1> : null}
-          {this.state.loading ? <Spinner /> : null }
+          : null}
+        {!this.state.actors && !this.state.loading ? <h1>No Movie Found</h1> : null}
+        {this.state.loading ? <Spinner /> : null}
       </div>
     )
   }
